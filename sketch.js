09344a881981fb08b8e0e96d31f791fbb8322829
Hui Shan <huiishan99@gmail.com>
2024-11-26 20:05:16 +0900

    Update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+
+// Create a 2D array
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
+        // Fill the array with 0s
         for (let j = 0; j < arr[i].length; j++) {
             arr[i][j] = 0;
         }
@@ -9,15 +12,19 @@ function make2DArray(cols, rows) {
     return arr;
 }
 
+// The grid
 let grid;
-let w = 10;
+// The big of each square
+let w = 5;
 let cols, rows;
 let hueValue = 200;
 
+// Check if a row is within the grid
 function withinCols(i) {
     return i >= 0 && i <= cols - 1;
 }
 
+// Check if a column is within the grid
 function withinRows(j) {
     return j >= 0 && j <= rows - 1;
 }
@@ -28,20 +35,13 @@ function setup() {
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = 0;
-        }
-    }
-
-    grid[20][10] = 1;
 }
 
 function mouseDragged() {
     let mouseCol = floor(mouseX / w);
     let mouseRow = floor(mouseY / w);
 
+    // Randomly add an area of sand particles
     let matrix = 3;
     let extent = floor(matrix / 2);
     for (let i = -extent; i <= extent; i++) {
@@ -55,8 +55,10 @@ function mouseDragged() {
             }
         }
     }
+
+    // Change the color of the sand over time
     hueValue += 1;
-    if(hueValue > 360) {
+    if (hueValue > 360) {
         hueValue = 1;
     }
 }
@@ -64,11 +66,12 @@ function mouseDragged() {
 function draw() {
     background(0);
 
+    // Draw the sand
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             noStroke();
             if (grid[i][j] > 0) {
-                 fill(grid[i][j], 255, 255);
+                fill(grid[i][j], 255, 255);
                 let x = i * w;
                 let y = j * w;
                 square(x, y, w);
@@ -76,34 +79,40 @@ function draw() {
         }
     }
 
+    // Create a 2D array for the next frame of animation
     let nextGrid = make2DArray(cols, rows);
+
+    // Check every cell and update the next grid
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let state = grid[i][j];
             if (state > 0) {
                 let below = grid[i][j + 1];
 
+                // Randomly fall to the left or right
                 let dir = 1;
                 if (random(1) < 0.5) {
                     dir *= -1;
                 }
 
-                let belowA, belowB;
-
-                if (i + dir >= 0 && i + dir <= cols - 1) {
+                // Check below left or right
+                let belowA = -1;
+                let belowB = -1;
+                if (withinCols(i + dir)) {
                     belowA = grid[i + dir][j + 1];
                 }
-                if (i - dir >= 0 && i - dir <= cols - 1) {
+                if (withinCols(i - dir)) {
                     belowB = grid[i - dir][j + 1];
                 }
+
                 if (below === 0) {
-                    nextGrid[i][j + 1] = grid[i][j];
+                    nextGrid[i][j + 1] = state;
                 } else if (belowA === 0) {
-                    nextGrid[i + dir][j + 1] = grid[i][j];
+                    nextGrid[i + dir][j + 1] = state;
                 } else if (belowB === 0) {
-                    nextGrid[i - dir][j + 1] = grid[i][j];
+                    nextGrid[i - dir][j + 1] = state;
                 } else {
-                    nextGrid[i][j] = grid[i][j];
+                    nextGrid[i][j] = state;
                 }
             }
         }
